69847572ae40e083db639affa2424aa284faf950
Bhanu <Bhanu@DESKTOP-RVI436K>
2025-05-10 05:37:33 -0700

    First Commit

---
+++ src/cap/com/Test1.java
@@ -0,0 +1,19 @@
+package cap.com;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String str="hello";
+		if(str=="hello") {
+		System.out.println("it's looks good");	
+		}
+		else {
+			System.out.println("it's looks not good");		
+		}
+	}
+
+
+
+	}
+
+
