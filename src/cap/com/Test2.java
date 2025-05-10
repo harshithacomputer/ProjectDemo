1f01ac7f0a92125b5943e6a5e082b86b98501f62
BHASKAR <BHASKAR@LAPTOP-O0LS0EJM>
2025-05-10 18:52:05 +0530

    Test3

---
+++ src/cap/com/Test2.java
@@ -0,0 +1,14 @@
+package cap.com;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String str = "hello";
+		if (str == "hello") {
+			System.out.println("it's looks good");
+		} else {
+			System.out.println("it's looks not good");
+		}
+	}
+
+}
---
+++ src/cap/com/Test3.java
@@ -0,0 +1,11 @@
+package cap.com;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		String s="hello";
+		System.out.println(s);
+
+	}
+
+}
